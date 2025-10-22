9b4ff4166949e4e0984d71eee59f0974ed297dbb
StanzTheDev <135062697+StanzTheDev@users.noreply.github.com>
2025-10-22 21:08:20 +0100

    Add null check for hamburger and navMenu

--- about.js
+++ about.js
@@ -1,6 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
-        const navMenu = document.querySelector('.nav-menu');
+  const navMenu = document.querySelector('.nav-menu');
 
-        hamburger.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-        });
+  if (hamburger && navMenu) {
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  }
+});
